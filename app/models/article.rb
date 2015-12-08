13ac0c18d02c3c7cfa9c7594331761170805bb3d
jaydacode <jaydacode@gmail.com>
2015-12-08 21:07:52 +0000

    Redid lessons 58 to 60

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
+#creating this model, rails automatically gives us getters and setters for each attr in the table.
 class Article < ActiveRecord::Base
   
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
